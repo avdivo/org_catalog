ce022620593d0427136c5a6ee9d21eefcef65df5
avdivo <avdivo@yahoo.com>
2025-02-16 17:16:10 +0300

    Тесты исправлены на асинхнонные

--- tests/test_org.py
+++ tests/test_org.py
@@ -1,17 +1,29 @@
-from fastapi.testclient import TestClient
+import pytest
+from httpx import ASGITransport, AsyncClient
 
-from app.main import app  # Импортируйте ваше приложение FastAPI
+from app.main import app
 from app.core.config import Config
 
-# Создаем TestClient
-client = TestClient(app)
+
+@pytest.mark.anyio
+async def test_root():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get("/api/v1/organizations/1")
+    assert response.status_code == 403
+    assert response.json() == {"detail": "Not authenticated"}
 
 
-def test_get_organization():
-    response = client.get("/api/v1/organizations/1", headers={"X-API-KEY": Config.API_KEY})
+@pytest.mark.asyncio
+@pytest.mark.parametrize("org_id, expected_name", [(1, "Мясной Двор")])
+async def test_get_organization(org_id, expected_name):
+    """Тестирование получения организаций по ID"""
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get(f"/api/v1/organizations/{org_id}", headers={"X-API-KEY": Config.API_KEY})
     assert response.status_code == 200
-    data = response.json()
-    assert data["name"] == "Мясной Двор"
-    assert data["building"]["address"] == "ул. Тверская, д. 10, Москва"
+    assert response.json()["name"] == expected_name
 
 
